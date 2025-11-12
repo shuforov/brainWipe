2a2a230d9e5eecd8ce8f696a4dc3be2ce3bb8006
shuforov <shuforov@gmail.com>
2025-11-12 19:10:49 +0200

    update alphabet ua add lowercase.

--- src/headers/scenes/mainMenuConstantsScene.h
+++ src/headers/scenes/mainMenuConstantsScene.h
@@ -13,47 +13,81 @@
 #define BORDER_BOTTOM_SIDE 0x17;
 
 // cursor selector buttons tiles
-#define LEFT_CURSOR 0x3C;
-#define RIGHT_CURSOR 0x3D;
-#define DOWN_CURSOR 0x40;
-#define UP_CURSOR 0x41;
-#define A_SELECTOR_BUTTON 0x3F;
-#define B_SELECTOR_BUTTON 0x3E;
+#define LEFT_CURSOR 0x5F;
+#define RIGHT_CURSOR 0x60;
+#define DOWN_CURSOR 0x63;
+#define UP_CURSOR 0x64;
+#define A_SELECTOR_BUTTON 0x62;
+#define B_SELECTOR_BUTTON 0x61;
 
-// alphabet ua tiles
+// alphabet ua tiles uppercase
 #define A 0x19; // А
 #define A0 0x1A; // Б
 #define A1 0x1B; // В
 #define A2 0x1C; // Г
 #define A3 0x1D; // Ґ
-#define A4 0x1E; // Д
-#define A5 0x1F; // Е
-#define A6 0x20; // Є
-#define A7 0x21; // Ж
-#define A8 0x22; // З
-#define A9 0x23; // И
-#define A10 0x24; // І
-#define A11 0x25; // Ї
-#define A12 0x26; // Й
-#define A13 0x27; // К
-#define A14 0x28; // Л
-#define A15 0x29; // М
-#define A16 0x2A; // Н
-#define A17 0x2B; // О
-#define A18 0x2C; // П
-#define A19 0x2D; // Р
-#define A20 0x2E; // С
-#define A21 0x2F; // Т
-#define A22 0x30; // У
-#define A23 0x31; // Ф
-#define A24 0x32; // Х
-#define A25 0x33; // Ц
-#define A26 0x34; // Ч
-#define A27 0x35; // Ш
-#define A28 0x36; // Щ
-#define A29 0x37; // Ь
-#define A30 0x38; // Ю
-#define A31 0x39; // Я
-#define A32 0x3A; // '
+#define A4 0x23; // Д
+#define A5 0x24; // Е
+#define A6 0x25; // Є
+#define A7 0x26; // Ж
+#define A8 0x27; // З
+#define A9 0x2D; // И
+#define A10 0x2E; // І
+#define A11 0x2F; // Ї
+#define A12 0x30; // Й
+#define A13 0x31; // К
+#define A14 0x37; // Л
+#define A15 0x38; // М
+#define A16 0x39; // Н
+#define A17 0x3A; // О
+#define A18 0x3B; // П
+#define A19 0x41; // Р
+#define A20 0x42; // С
+#define A21 0x43; // Т
+#define A22 0x44; // У
+#define A23 0x45; // Ф
+#define A24 0x4B; // Х
+#define A25 0x4C; // Ц
+#define A26 0x4D; // Ч
+#define A27 0x4E; // Ш
+#define A28 0x4F; // Щ
+#define A29 0x55; // Ь
+#define A30 0x56; // Ю
+#define A31 0x57; // Я
+#define A32 0x58; // '
+// alphabet ua tiles lowercase
+#define A33 0x1E // a
+#define A34 0x1F // б
+#define A35 0x20 // в
+#define A36 0x21 // г
+#define A37 0x22 // ґ
+#define A38 0x2A // д
+#define A39 0x28 // е
+#define A40 0x29 // є
+#define A41 0x2B // ж
+#define A42 0x2C // з
+#define A43 0x32 // и
+#define A44 0x33 // і
+#define A45 0x34 // ї
+#define A46 0x35 // й
+#define A47 0x36 // к
+#define A48 0x3C // л
+#define A49 0x3D // м
+#define A50 0x3E // н
+#define A51 0x3F // о
+#define A52 0x40 // п
+#define A53 0x46 // р
+#define A54 0x47 // с
+#define A55 0x48 // т
+#define A56 0x49 // у
+#define A57 0x4A // ф
+#define A58 0x50 // х
+#define A59 0x51 // ц
+#define A60 0x52 // ч
+#define A61 0x53 // ш
+#define A62 0x54 // щ
+#define A63 0x5A // ь
+#define A64 0x5B // ю
+#define A65 0x5C // я
 
 #endif
